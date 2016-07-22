4aa056c9e0752220014f3b60cb6cd5a651ef7882
Jonny B <jonnyb@Jonnys-MacBook-Pro.local>
2016-07-21 22:16:54 -0700

    Lesson 2 Code

---
+++ pokedex2/Pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon.swift
+//  pokedex2
+//
+//  Created by Jonny B on 7/21/16.
+//  Copyright © 2016 Jonny B. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+}
